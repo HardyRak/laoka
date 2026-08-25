66b13ceb72ffc5e4972f1b7ee3656e22969a6dcb
HardyRak <hardyrakoto66@gmail.com>
2026-08-25 17:32:21 +0300

    create side dish type and mock data

---
+++ src/app/types/side-dish.type.ts
@@ -0,0 +1,83 @@
+export interface SideDish {
+  id: string;
+  emojy: string;
+  name: string;
+}
+
+export const sideDishes: SideDish[] = [
+  {
+    id: '1',
+    emojy: '🍟',
+    name: 'French Fries',
+  },
+  {
+    id: '2',
+    emojy: '🥗',
+    name: 'Green Salad',
+  },
+  {
+    id: '3',
+    emojy: '🍚',
+    name: 'Steamed Rice',
+  },
+  {
+    id: '4',
+    emojy: '🥔',
+    name: 'Mashed Potatoes',
+  },
+  {
+    id: '5',
+    emojy: '🧅',
+    name: 'Onion Rings',
+  },
+  {
+    id: '6',
+    emojy: '🌽',
+    name: 'Grilled Corn',
+  },
+  {
+    id: '7',
+    emojy: '🥕',
+    name: 'Roasted Vegetables',
+  },
+  {
+    id: '8',
+    emojy: '🫘',
+    name: 'Baked Beans',
+  },
+  {
+    id: '9',
+    emojy: '🥒',
+    name: 'Pickles',
+  },
+  {
+    id: '10',
+    emojy: '🥦',
+    name: 'Steamed Broccoli',
+  },
+  {
+    id: '11',
+    emojy: '🍠',
+    name: 'Sweet Potato',
+  },
+  {
+    id: '12',
+    emojy: '🥖',
+    name: 'Garlic Bread',
+  },
+  {
+    id: '13',
+    emojy: '🍝',
+    name: 'Pasta Salad',
+  },
+  {
+    id: '14',
+    emojy: '🥑',
+    name: 'Avocado Salad',
+  },
+  {
+    id: '15',
+    emojy: '🍅',
+    name: 'Tomato Salad',
+  },
+];
