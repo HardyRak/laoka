0225d4bd6c4ab03c73d0d26e23516897447be243
HardyRak <hardyrakoto66@gmail.com>
2026-08-25 17:32:09 +0300

    add choise groupe in profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { PageHeaderComponent } from '../../component/page-header/page-header.component';
+import { NameFieldComponent } from '../../component/name-field/name-field.component';
+import { ChoiceGroupComponent } from '../../component-container/choice-group/choice-group.component';
+import { sideDishes } from '../../types/side-dish.type';
+import { ChoiceChipComponent } from '../../component/choice-chip/choice-chip.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
-  imports: [],
+  imports: [
+    PageHeaderComponent,
+    NameFieldComponent,
+    ChoiceGroupComponent,
+    ChoiceChipComponent,
+  ],
 })
 export class ProfileComponent implements OnInit {
+  protected sideDish = sideDishes;
+
   constructor() {}
 
   ngOnInit() {}
