31b9d207e7d74e133e52afa11db6d8d9152de43d
HardyRak <hardyrakoto66@gmail.com>
2026-08-25 15:40:51 +0300

    create 3 pages for all featiures

--- src/app/layout/main-layout/main-layout.routes.ts
+++ src/app/layout/main-layout/main-layout.routes.ts
@@ -5,6 +5,28 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./main-layout.component').then((m) => m.MainLayoutComponent),
-    children: [],
+    children: [
+      {
+        path: 'today',
+        loadComponent: () =>
+          import('../../pages/today/today.component').then(
+            (m) => m.TodayComponent,
+          ),
+      },
+      {
+        path: 'calendar',
+        loadComponent: () =>
+          import('../../pages/calendar/calendar.component').then(
+            (m) => m.CalendarComponent,
+          ),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('../../pages/profile/profile.component').then(
+            (m) => m.ProfileComponent,
+          ),
+      },
+    ],
   },
 ];
---
+++ src/app/pages/today/today.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.scss'],
+  imports: [],
+})
+export class TodayComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
