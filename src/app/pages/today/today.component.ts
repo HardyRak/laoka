c8e30a78730ba451e5737146429fbc6741729220
HardyRak <hardyrakoto66@gmail.com>
2026-08-26 16:51:18 +0300

    feat(today): integrate dish of day card component and add nutrition data

--- src/app/pages/today/today.component.ts
+++ src/app/pages/today/today.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular';
 import { PageHeaderComponent } from '../../component/page-header/page-header.component';
+import { DishOfDayCardComponent } from '../../component/dish-of-day-card/dish-of-day-card.component';
+import { nutrition } from '../../types/dish-nutrition-stat';
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
   styleUrls: ['./today.component.scss'],
-  imports: [IonContent, PageHeaderComponent],
+  imports: [IonContent, PageHeaderComponent, DishOfDayCardComponent],
 })
 export class TodayComponent implements OnInit {
+  nutrition = nutrition;
+
   constructor() {}
 
   ngOnInit() {}
