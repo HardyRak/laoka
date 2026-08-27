43163a1a3bbd79552807f3aced94ea44dd6d5892
HardyRak <hardyrakoto66@gmail.com>
2026-08-27 23:46:15 +0300

    feat(onboarding-layout): enhance layout structure with content wrapping and add PageHeader component

--- src/app/layout/onboarding-layout/onboarding-layout.component.ts
+++ src/app/layout/onboarding-layout/onboarding-layout.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { IonRouterOutlet } from '@ionic/angular';
+import { IonRouterOutlet, IonContent } from '@ionic/angular';
 import { WizardFooterComponent } from '../../component-container/wizard-footer/wizard-footer.component';
 import { WizardHeaderComponent } from '../../component/wizard-header/wizard-header.component';
+import { PageHeaderComponent } from '../../component/page-header/page-header.component';
 
 @Component({
   selector: 'app-onboarding-layout',
   templateUrl: './onboarding-layout.component.html',
   styleUrls: ['./onboarding-layout.component.scss'],
-  imports: [IonRouterOutlet, WizardFooterComponent, WizardHeaderComponent],
+  imports: [
+    IonRouterOutlet,
+    WizardFooterComponent,
+    WizardHeaderComponent,
+    PageHeaderComponent,
+    IonContent,
+  ],
 })
 export class OnboardingLayoutComponent implements OnInit {
   constructor() {}
