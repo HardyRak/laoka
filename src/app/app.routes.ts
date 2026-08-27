60335b33d12f2254d6c97717a9a7a27b463c9be0
HardyRak <hardyrakoto66@gmail.com>
2026-08-27 22:44:04 +0300

    fix(routes): update loadChildren path for layout routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./layout/main-layout/main-layout.routes').then((m) => m.routes),
+    loadChildren: () => import('./layout/layout.route').then((m) => m.routes),
   },
 ];
